55dd9fcc350ef68ad87b219d2d63e8156d342073
BinuxLiu <binuxliu@gmail.com>
2023-07-26 11:49:36 +0800

    add the tokyo247 dataset

--- format_datasets.py
+++ format_datasets.py
@@ -5,6 +5,8 @@ import utm
 from glob import glob
 from tqdm import tqdm
 from scipy.io import loadmat
+from PIL import Image
+import torchvision
 
 def format_coord(num, left=2, right=5):
     """Return the formatted number as a string with (left) int digits 
@@ -65,13 +67,13 @@ class Format_Datasets():
         self.testB_path = os.path.join(self.nightstreet_path, "testB")
 
         self.tokyo247_path = os.path.join(self.datasets_folder, "tokyo247")
-        self.pitts30k_n_path = os.path.join(self.datasets_folder, "pitts30k")
+        self.pitts30k_path = os.path.join(self.datasets_folder, "pitts30k")
 
         os.makedirs(self.raw_pittsburgh_dataset_path, exist_ok=True)
         os.makedirs(self.raw_tokyo_dataset_path, exist_ok=True)
 
         os.makedirs(self.tokyo247_path, exist_ok=True)
-        os.makedirs(self.pitts30k_n_path, exist_ok=True) 
+        os.makedirs(self.pitts30k_path, exist_ok=True) 
 
         os.makedirs(self.trainA_path, exist_ok=True)
         os.makedirs(self.trainB_path, exist_ok=True)
@@ -130,7 +132,7 @@ class Format_Datasets():
 
     def build_sf_xl_small(self):
         shutil.unpack_archive(os.path.join(self.raw_dataset_path, "small.zip"), self.datasets_folder)
-        os.rename(os.path.join(self.datasets_folder, 'small'), os.path.join(self.datasets_folder, "sf_xl_small_n"))
+        os.rename(os.path.join(self.datasets_folder, 'small'), os.path.join(self.datasets_folder, "sf_xl_small"))
 
 
     def copy_pitts_images(self, target_folder, images_paths, utms):
@@ -156,15 +158,62 @@ class Format_Datasets():
             # Database
             g_images = [f[0].item() for f in mat_struct[1]]
             g_utms = mat_struct[2].T
-            self.copy_pitts_images(os.path.join(self.pitts30k_n_path, dataset, 'database'), g_images, g_utms)
+            self.copy_pitts_images(os.path.join(self.pitts30k_path, dataset, 'database'), g_images, g_utms)
             # Queries
             q_images = [os.path.join("queries_real", f"{f[0].item()}") for f in mat_struct[3]]
             q_utms = mat_struct[4].T
-            self.copy_pitts_images(os.path.join(self.pitts30k_n_path, dataset, 'queries'), q_images, q_utms)
+            self.copy_pitts_images(os.path.join(self.pitts30k_path, dataset, 'queries'), q_images, q_utms)
 
     
     def build_tokyo247(self):
-        pass
+        matlab_struct_file_path = os.path.join(self.raw_tokyo_dataset_path, 'index', 'tokyo247.mat')
+        mat_struct = loadmat(matlab_struct_file_path)["dbStruct"].item()
+        db_images = [os.path.join('tokyo', f[0].item().replace('.jpg', '.png')) for f in mat_struct[1]]
+        db_utms = mat_struct[2].T
+        dst_folder = os.path.join(self.tokyo247_path, 'images', 'test', 'database')
+
+        os.makedirs(dst_folder, exist_ok=True)
+        for src_image_path, (utm_east, utm_north) in zip(tqdm(db_images, desc=f"Copy to {dst_folder}", ncols=100),
+                                                        db_utms):
+            src_image_name = os.path.basename(src_image_path)
+            latitude, longitude = utm.to_latlon(utm_east, utm_north, 54, 'S')
+            pano_id = src_image_name[:22]
+            tile_num = int(re.findall('_012_(\d+)\.png', src_image_name)[0])//30
+            assert 0 <= tile_num < 12
+            dst_image_name = format_image_name(latitude, longitude, pano_id=pano_id,
+                                                    tile_num=tile_num)
+            src_image_path = f"{self.raw_dataset_path}/{src_image_path}"
+            try:
+                Image.open(src_image_path).save(f"{dst_folder}/{dst_image_name}")
+            except OSError as e:
+                print("\n")
+                print(f"Exception {e} with file {src_image_path}")
+                raise e
+            
+        filename = "247query_subset_v2.zip"
+        # url = f"https://data.ciirc.cvut.cz/public/projects/2015netVLAD/Tokyo247/queries/{filename}"
+        file_zip_path = os.path.join(self.raw_dataset_path, filename)
+        # util.download_heavy_file(url, file_zip_path)
+        shutil.unpack_archive(file_zip_path, os.path.join(self.raw_dataset_path, "tokyo"))
+        src_queries_folder = os.path.join(self.raw_dataset_path, "tokyo", filename).replace(".zip", "")
+        src_queries_paths = sorted(glob(os.path.join(src_queries_folder, "*.jpg")))
+        os.makedirs(os.path.join(self.tokyo247_path, "images", "test", "queries"), exist_ok=True)
+        for src_query_path in tqdm(src_queries_paths, desc=f"Copy to {self.tokyo247_path}/images/test/queries", ncols=100):
+            csv_path = src_query_path.replace(".jpg", ".csv")
+            with open(csv_path, "r") as file:
+                info = file.readline()
+            pano_id, latitude, longitude = info.split(",")[:3]
+            pano_id = pano_id.replace(",jpg", "")
+            dst_image_name = format_image_name(latitude, longitude, pano_id=pano_id)
+            dst_image_path = os.path.join(self.tokyo247_path, "images", "test", "queries", dst_image_name)
+            try:
+                pil_img = Image.open(src_query_path)
+            except OSError as e:
+                print(f"Exception {e} with file {src_query_path}")
+                raise e
+            resized_pil_img = torchvision.transforms.Resize(480)(pil_img)
+            resized_pil_img.save(dst_image_path)
+
 
 
     def build_aachenDN(self):
@@ -176,7 +225,8 @@ if __name__ == "__main__":
     # formater.build_nightcity()
     # formater.tokyo247_to_nightstreet()
     # formater.aachenDN_to_nightstreet()
-    formater.build_sf_xl_small()
+    # formater.build_sf_xl_small()
     # formater.build_pitts30k()
+    formater.build_tokyo247()
 
 
